4125092eaeb99f992be29fc976b4642c0b7096a6
Neal Richardson <neal.p.richardson@gmail.com>
2023-07-07 08:44:18 -0400

    Move test helper functions to helper.R (closes #23)

---
+++ tests/testthat/helper.R
@@ -0,0 +1,38 @@
+capture_while_mocking <- function(..., path) {
+  with_mock_path(path, {
+    # We'll write to `path` but read from wherever was set before
+    tracer <- quote({
+      .mockPaths <- function() getOption("httptest2.mock.paths")[-1]
+    })
+    with_trace("find_mock_file",
+      where = with_mock_api, tracer = tracer,
+      expr = capture_requests(...)
+    )
+  })
+}
+
+with_redactor <- function(x, ...) {
+  old <- getOption("httptest2.redactor")
+  old.pkgs <- getOption("httptest2.redactor.packages")
+  set_redactor(x)
+  on.exit({
+    set_redactor(old)
+    options(httptest2.redactor.packages = old.pkgs)
+  })
+  eval.parent(...)
+}
+
+reset_redactors <- function() {
+  options(
+    httptest2.redactor = NULL,
+    httptest2.redactor.packages = NULL
+  )
+}
+
+install_testpkg <- function(pkg, lib = tempfile()) {
+  dir.create(lib)
+  tools::Rcmd(c("INSTALL", "testpkg", paste0("--library=", shQuote(lib))),
+    stdout = NULL, stderr = NULL
+  )
+  return(lib)
+}
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -7,52 +7,6 @@ options(
 
 library(httr2, warn.conflicts = FALSE)
 
-capture_while_mocking <- function(..., path) {
-  with_mock_path(path, {
-    # We'll write to `path` but read from wherever was set before
-    tracer <- quote({
-      .mockPaths <- function() getOption("httptest2.mock.paths")[-1]
-    })
-    with_trace("find_mock_file",
-      where = with_mock_api, tracer = tracer,
-      expr = capture_requests(...)
-    )
-  })
-}
-
-with_redactor <- function(x, ...) {
-  old <- getOption("httptest2.redactor")
-  old.pkgs <- getOption("httptest2.redactor.packages")
-  set_redactor(x)
-  on.exit({
-    set_redactor(old)
-    options(httptest2.redactor.packages = old.pkgs)
-  })
-  eval.parent(...)
-}
-
-reset_redactors <- function() {
-  options(
-    httptest2.redactor = NULL,
-    httptest2.redactor.packages = NULL
-  )
-}
-
-install_testpkg <- function(pkg, lib = tempfile()) {
-  dir.create(lib)
-  tools::Rcmd(c("INSTALL", "testpkg", paste0("--library=", shQuote(lib))),
-    stdout = NULL, stderr = NULL
-  )
-  return(lib)
-}
-
-skip_on_R_older_than <- function(version) {
-  r <- R.Version()
-  if (utils::compareVersion(paste(r$major, r$minor, sep = "."), version) < 0) {
-    skip(paste("Requires R >=", version))
-  }
-}
-
 httpbin <- webfakes::local_app_process(
   webfakes::httpbin_app(),
   .local_envir = testthat::teardown_env()

8abad34f5217190e6afee005fa7b50d64b7d4624
Neal Richardson <neal.p.richardson@gmail.com>
2023-11-07 10:05:51 -0500

    Fix note about utils::

--- R/expect-request-header.R
+++ R/expect-request-header.R
@@ -87,7 +87,7 @@ expect_request_header <- function(expr,
 
 with_mock <- function(mock, code) {
   if (httr2_1.0.0) {
-    getFromNamespace("with_mocked_responses", "httr2")(mock, code)
+    utils::getFromNamespace("with_mocked_responses", "httr2")(mock, code)
   } else {
     httr2::with_mock(mock, code)
   }
